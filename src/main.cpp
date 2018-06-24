12e1354b710a13dcda5c7559862c488fa69a0c39
lmontigny <laurent.montigny@gmail.com>
2018-06-24 20:38:52 +0200

    added time measurement

--- src/main.cpp
+++ src/main.cpp
@@ -6,6 +6,14 @@
 #include <iostream>
 #include <memory>
 #include <cblas.h>
+#include <chrono>
+
+#include <sys/time.h>
+#include <time.h>
+
+
+using namespace std::chrono;
+
 
 
 double doubleRandomGenerator(){
@@ -29,7 +37,7 @@ int main(int argc, char const *argv[]){
 	double *matrix_A, *matrix_B, *matrix_C;
 	//std::unique_ptr<double> matrix_A = std::make_unique<double>();
 
-	dim = 64;
+	dim = 512;
 	matrix_A = (double *)malloc( dim*dim*sizeof( double ));
 	matrix_B = (double *)malloc( dim*dim*sizeof( double ));
 	matrix_C = (double *)malloc( dim*dim*sizeof( double ));
@@ -43,6 +51,7 @@ int main(int argc, char const *argv[]){
 		matrix_B[i]=doubleRandomGenerator();
 	}
 
+high_resolution_clock::time_point t1 = high_resolution_clock::now();
 	for(int i=0; i<dim; i++){  // row
 		for(int j=0; j<dim; j++){ // col
 				double sum = 0;
@@ -52,15 +61,33 @@ int main(int argc, char const *argv[]){
 				matrix_C[i*dim+j] = sum;
 		}
 	}
+high_resolution_clock::time_point t2 = high_resolution_clock::now();
+    //auto duration = duration_cast<microseconds>( t2 - t1 ).count();
 
-	printMatrix(dim, matrix_C);
+    //std::cout << duration << std::endl;
 
+	//printMatrix(dim, matrix_C);
+
+  struct timeval start,finish;
+  double duration;
+
+gettimeofday(&start, NULL);
 	cblas_dgemm(CblasColMajor, CblasNoTrans, CblasNoTrans, dim, dim, dim, 1.0, matrix_A, dim, matrix_B, dim, 1.0, matrix_C, dim);
+  gettimeofday(&finish, NULL);
+
+  duration = ((double)(finish.tv_sec-start.tv_sec)*1000000 + (double)(finish.tv_usec-start.tv_usec)) / 1000000;
+  double gflops = 2.0 * dim * dim * dim;
+  gflops = gflops/duration*1.0e-6;
+
+std::cout << gflops << " " << duration;
+
 
 
 	free(matrix_A);
 	free(matrix_B);
 	free(matrix_C);
+
+	return 0;
 }
 
 
